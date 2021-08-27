c2dd05dd265a4bb4e6d7b0eb89f4125784264dda
Leander Lukas <leander.lukas@t-online.de>
2021-08-27 17:26:05 +0200

    Erstelle Klassengerüst

---
+++ sent_parse_iter.py
@@ -0,0 +1,25 @@
+#      Python: 3.7.6
+#   Kodierung: utf-8
+#        Name: Leander Lukas
+# Matrikelnr.: 802559
+""" """
+import os
+import nltk.tree
+import logging
+
+
+logging.basicConfig(filename='out.log', filemode='w', level=logging.DEBUG)
+
+
+class SentParseIter():
+    """ """
+    def __init__(path):
+        self.path = path
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
