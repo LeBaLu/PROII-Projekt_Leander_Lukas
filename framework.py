4033b854ba63da4f77777982340be5fdd45cae4e
Leander Lukas <leander.lukas@t-online.de>
2021-08-30 17:54:56 +0200

    Implementiere Breitensuche in Bäumen

---
+++ framework.py
@@ -0,0 +1,23 @@
+#      Python: 3.7.6
+#   Kodierung: utf-8
+#        Name: Leander Lukas
+# Matrikelnr.: 802559
+""" """
+from sieve import AbstractSieve
+import logging
+
+
+logging.basicConfig(filename='out.log', filemode='w', level=logging.INFO)
+
+
+class SieveFramework(AbstractSieve):
+    """ """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- sieve.py
+++ sieve.py
@@ -3,20 +3,53 @@
 #        Name: Leander Lukas
 # Matrikelnr.: 802559
 """Abstrakte Siebklasse."""
-from abc import ABC, abstractmethod
 from nltk.tree import Tree
+from sent_parse_iter import SentParseGen
 import logging
 
 
-class AbstractSieve(ABC):
+logging.basicConfig(filename='out.log', filemode='w', level=logging.INFO)
+
+
+class AbstractSieve():
     """ """
-    def __init__(self, mentions, clusters):
+    def __init__(self, path, mentions=[],
+                 clusters=[], mention_to_cluster=dict()
+                                        ):
+        self.generator = SentParseGen(path)
         self.mentions = mentions
         self.clusters = clusters
+        self.mention_to_cluster = mention_to_cluster
+
+    @staticmethod
+    def breadth_first_tree_traversal(tree, label_in=['NP', 'NX'], left_to_right=True):
+        """ """
+        # Next node to be expanded.
+        act_node = tree
+        frontier = [tree]
+        trees_out = []
+        if left_to_right:
+            step_size = 1
+        else:
+            step_size = -1
+        while frontier:
+            act_node = frontier[0]
+            if type(act_node) is Tree:
+                if act_node.label() in label_in:
+                    trees_out.append(act_node)
+                for child in act_node[::step_size]:
+                    logging.info(frontier)
+                    frontier.append(child)
+            frontier.pop(0)
+        return trees_out
 
 
 def main():
-    pass
+    obj1 = AbstractSieve("flat_train_2012/bc_cctv_0001.v4_auto_conll")
+    tree = Tree('S', [Tree('NP', ['the', 'cat']), Tree('VP', ['ate'])])
+    # Right to left breadth first traversal of NPs in tree.
+    NPs1 = AbstractSieve.breadth_first_tree_traversal(tree, left_to_right=False)
+    logging.info(NPs1)
 
 
 if __name__ == "__main__":
