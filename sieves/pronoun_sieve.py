ba095638364eabca9ee0c88020447766699c06bf
Leander Lukas <leander.lukas@t-online.de>
2021-09-02 17:19:00 +0200

    Erstelle Klassengerüst

---
+++ sieves/pronoun_sieve.py
@@ -0,0 +1,26 @@
+#      Python: 3.7.6
+#   Kodierung: utf-8
+#        Name: Leander Lukas
+# Matrikelnr.: 802559
+""" """
+from sieve import AbstractSieve
+import logging
+
+
+logging.basicConfig(filename='out.log', filemode='w', level=logging.INFO)
+
+
+class PronounSieve(AbstractSieve):
+    """Sieve inspired by Ragunathan et al.."""
+    def apply_sieve(self):
+        """ """
+        pass
+
+
+def main():
+    # Siehe framework.py für Demonstrationen.
+    pass
+
+
+if __name__ == "__main__":
+    main()
