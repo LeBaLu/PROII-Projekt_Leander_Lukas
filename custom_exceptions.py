af0ad21d1d2ce9aad2b8cf7757fb41cb4c9dd8d1
Leander Lukas <leander.lukas@t-online.de>
2021-09-03 21:03:40 +0200

    Stelle SieveNotImplementedError zur Verfügung

---
+++ custom_exceptions.py
@@ -0,0 +1,29 @@
+#      Python: 3.7.6
+#   Kodierung: utf-8
+#        Name: Leander Lukas
+# Matrikelnr.: 802559
+"""Selbstbenannte Fehlerklassen.
+
+Hier sind die Ausnahmen, die bei fehlerhaftem oder unvollständigem Input
+geworfen werden sollen, definiert.
+
+"""
+class SieveNotImplementedError(Exception):
+    """Unknown-sieve-exception.
+
+    Should be raised when unknown sieve-IDs are passed by the user.
+
+    Arg:
+        msg(str): Human readable string describing the exception.
+
+    """
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
