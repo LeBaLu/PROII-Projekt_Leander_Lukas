194386e227126e78fb2cba570e8e30f2394d620d
Leander Lukas <leander.lukas@t-online.de>
2021-09-03 14:21:17 +0200

    Erstelle Klassengerüst

---
+++ cluster_evaluation.py
@@ -0,0 +1,24 @@
+#      Python: 3.7.6
+#   Kodierung: utf-8
+#        Name: Leander Lukas
+# Matrikelnr.: 802559
+""" """
+import logging
+
+
+logging.basicConfig(filename='out.log', filemode='w', level=logging.INFO)
+
+
+class ConllClusterEvaluator():
+    """ """
+    def __init__(self, path):
+        self.path = path
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
